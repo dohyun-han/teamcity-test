1ac9167ce9fe4ff2f4fd2960ba58cfce993e7f86
dohyeon-han <limited.hyeon@gmail.com>
2024-05-26 00:39:37 +0900

    key 값 변환

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import os
 import paramiko
+from io import StringIO
 
 server_ip = '127.0.0.1'
 username = 'han'
-ssh_key = os.getenv('env.SSH_key')
+ssh_key_str = os.getenv('env.SSH_key')
+ssh_key_fileobj = StringIO(ssh_key_str)
+ssh_key = paramiko.RSAKey.from_private_key(ssh_key_fileobj)
 
 def connect_ssh():
     ssh_client = paramiko.SSHClient()
