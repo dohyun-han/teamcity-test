744e8493cd6a6562734b37c60dac16b8adbc4985
dohyeon-han <limited.hyeon@gmail.com>
2024-05-26 01:08:09 +0900

    파일 생성

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from io import StringIO
 server_ip = '172.22.158.9'
 username = 'han'
 ssh_key_str = os.getenv('SSH_key')
-password = os.getenv('password')
 ssh_key_fileobj = StringIO(ssh_key_str)
 ssh_key = paramiko.RSAKey.from_private_key(ssh_key_fileobj)
 
@@ -15,5 +14,15 @@ def connect_ssh():
     ssh_client.connect(server_ip, port=22, username=username, pkey=ssh_key)
     return ssh_client
 
-client = connect_ssh()
-client.close()
+def create_file(filename, content):
+    # 연결된 SSH 클라이언트 객체 가져오기
+    client = connect_ssh()
+    try:
+        # 원하는 파일을 생성하고 내용을 쓰기
+        stdin, stdout, stderr = client.exec_command(f'echo "{content}" > {filename}')
+        print(stdout.read().decode())
+        print(stderr.read().decode())
+    finally:
+        client.close()
+
+create_file('test_file.txt', 'This is a test file created using SSH!')
