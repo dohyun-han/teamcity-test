7bb2909e141a47de48ab8653888d7dbd01cb532c
dohyeon-han <limited.hyeon@gmail.com>
2024-05-26 00:30:30 +0900

    ssh 연결

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import os
+import paramiko
 
+server_ip = '127.0.0.1'
+username = 'han'
 ssh_key = os.getenv('SSH_key')
-print(ssh_key)
+
+def connect_ssh(server_ip, username):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(server_ip, port=22, username=username, pkey=ssh_key)
+    return ssh_client
+
+ssh_client = connect_ssh(server_ip, username)
+ssh_client.close()
