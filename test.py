9441a857ff1894747097af7e424cd795e2e2826d
dohyeon-han <limited.hyeon@gmail.com>
2024-05-25 23:55:58 +0900

    제거

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
 import os
-import paramiko
-from io import StringIO
 
-ssh_key_content = os.getenv('SSH_key')
-
-if not ssh_key_content:
-    raise ValueError("MY_SSH_KEY.")
-
-key_file = StringIO(ssh_key_content)
-ssh_key = paramiko.RSAKey.from_private_key(key_file)
-
-hostname = "your.server.com"
-port = 22
-username = "your_username"
-
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+ssh_key = os.getenv('SSH_key')
 print(ssh_key)
