41d421e2389e0130c55c46f611f4ec81da892985
dohyeon-han <limited.hyeon@gmail.com>
2024-05-26 00:57:31 +0900

    password로 접속

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ from io import StringIO
 server_ip = '172.22.158.9'
 username = 'han'
 ssh_key_str = os.getenv('SSH_key')
+password = os.getenv('password')
 ssh_key_fileobj = StringIO(ssh_key_str)
 ssh_key = paramiko.RSAKey.from_private_key(ssh_key_fileobj)
 
 def connect_ssh():
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(server_ip, port=22, username=username, pkey=ssh_key)
+    ssh_client.connect(server_ip, port=22, username=username, password=password)
     return ssh_client
 
 client = connect_ssh()
