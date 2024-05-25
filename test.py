4f8bef8e8df0768ae3c61de78d8a7843d9054de7
dohyeon-han <limited.hyeon@gmail.com>
2024-05-26 00:36:33 +0900

    변수 이름 수정

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import paramiko
 
 server_ip = '127.0.0.1'
 username = 'han'
-ssh_key = os.getenv('SSH_key')
+ssh_key = os.getenv('env.SSH_key')
 
-def connect_ssh(server_ip, username):
+def connect_ssh():
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(server_ip, port=22, username=username, pkey=ssh_key)
     return ssh_client
 
-ssh_client = connect_ssh(server_ip, username)
-ssh_client.close()
+client = connect_ssh()
+client.close()
